c9ed4ee5b8654604238dab1266250ed39e51295d
Joshua Liu <122531548+joshl17@users.noreply.github.com>
2024-04-29 13:26:52 +0100

    Commented out self-creation of SSL cert and used import function instead

--- acm.tf
+++ acm.tf
@@ -1,32 +1,32 @@
-resource "tls_private_key" "quadzig" {
-  algorithm = "RSA"
-}
+#resource "tls_private_key" "quadzig" {
+#  algorithm = "RSA"
+#}
 
-resource "tls_self_signed_cert" "quadzig-ssl" {
-  key_algorithm   = "RSA"
-  private_key_pem = tls_private_key.quadzig.private_key_pem
+#resource "tls_self_signed_cert" "quadzig-ssl" {
+#  key_algorithm   = "RSA"
+#  private_key_pem = tls_private_key.quadzig.private_key_pem
 
-  subject {
-    common_name  = "quadzig.dev.internal"
-  }
+#  subject {
+#    common_name  = "quadzig.dsa.internal"
+#  }
 
-  validity_period_hours = 12
+#  validity_period_hours = 12
 
-  allowed_uses = [
-    "key_encipherment",
-    "digital_signature",
-    "server_auth",
-  ]
-}
+#  allowed_uses = [
+#    "key_encipherment",
+#    "digital_signature",
+#    "server_auth",
+#  ]
+#}
 
 #Import newly created SSL to ACM.
-resource "aws_acm_certificate" "cert" {
-  private_key      = tls_private_key.quadzig.private_key_pem
-  certificate_body = tls_self_signed_cert.quadzig-ssl.cert_pem
-}
+#resource "aws_acm_certificate" "cert" {
+#  private_key      = tls_private_key.quadzig.private_key_pem
+#  certificate_body = tls_self_signed_cert.quadzig-ssl.cert_pem
+#}
 
 #If we'd rather just import the existing cert
-#import {
-#  to = aws_acm_certificate.cert
-#  id = "arn:aws:acm:eu-central-1:123456789012:certificate/7e7a28d2-163f-4b8f-b9cd-822f96c08d6a"
-#}
+import {
+  to = aws_acm_certificate.cert
+  id = "arn:aws:acm:eu-west-2:500371445067:certificate/f0f11fa2-41c0-43c7-a41a-832970d50fec"
+}
