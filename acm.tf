3a97e8cc137e96477976090ea9fd21a8175a9fbc
Joshua Liu <122531548+joshl17@users.noreply.github.com>
2024-04-29 03:51:28 +0100

    Added existing import config (If required).

--- acm.tf
+++ acm.tf
@@ -24,3 +24,9 @@ resource "aws_acm_certificate" "cert" {
   private_key      = tls_private_key.quadzig.private_key_pem
   certificate_body = tls_self_signed_cert.quadzig-ssl.cert_pem
 }
+
+#If we'd rather just import the existing cert
+#import {
+#  to = aws_acm_certificate.cert
+#  id = "arn:aws:acm:eu-central-1:123456789012:certificate/7e7a28d2-163f-4b8f-b9cd-822f96c08d6a"
+#}
